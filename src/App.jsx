0829e7dbaeecd48071b63f136bef7cbb75c67d21
Danny Berrios <denny_berrios@hotmail.com>
2024-12-13 21:56:26 -0300

    Importando style global e page home

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
+import { GlobalStyle } from "./components/UI/GlobalStyle"
+import Home from "./views/Home"
+
 function App() {
   return (
     <>
+      <GlobalStyle/>
+      <Home />
     </>
   )
 }
