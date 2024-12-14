fc809120251ea3d7e81e8ec5728efced6ae977ab
Danny Berrios <denny_berrios@hotmail.com>
2024-12-13 21:28:08 -0300

    Adição do coponente container main

---
+++ src/components/Container/styles.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const Container = styled.main`
+    width: calc(100% -10px);
+    min-height: 100vh;
+    padding: 0 10px;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+`;
