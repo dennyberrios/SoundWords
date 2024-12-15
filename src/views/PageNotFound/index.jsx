10413705b0c071ff7c8399185be4da85790ceea2
Danny Berrios <denny_berrios@hotmail.com>
2024-12-14 23:36:15 -0300

    Criar componente PageNotFound: Exibe uma mensagem de erro 404 para rotas inválidas

---
+++ src/views/PageNotFound/index.jsx
@@ -0,0 +1,12 @@
+import { Container } from "../../components/Container/styles";
+
+const PageNotFound = () => {
+    return (
+        <Container>
+            <h1>404</h1>
+            <p>Sorry, the page you are looking for does not exist.</p>
+        </Container>
+    );
+};
+
+export default PageNotFound;
