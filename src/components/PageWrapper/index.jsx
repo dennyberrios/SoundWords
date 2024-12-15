cef47d8b179f29933e4f3bdc009619a38f7f39c9
Danny Berrios <denny_berrios@hotmail.com>
2024-12-14 23:38:12 -0300

    Criar componente PageWrapper: Renderiza o Header para rotas válidas e PageNotFound para rotas inválidas

---
+++ src/components/PageWrapper/index.jsx
@@ -0,0 +1,33 @@
+import { useLocation } from "react-router-dom";
+import Header from "../Header";
+import PageNotFound from "../../views/PageNotFound";
+import PropTypes from "prop-types";
+
+const PageWrapper = ({ children }) => {
+    const location = useLocation();
+
+    // List of defined routes
+    const validRoutes = ["/"];
+
+    // Check that the current route is among the valid ones  
+    const isValidRoute = validRoutes.includes(location.pathname);
+
+    return (
+        <>
+            {isValidRoute && <Header/>}
+            {
+                isValidRoute ? (
+                    children
+                ) : (
+                    <PageNotFound />
+                )
+            }
+        </>
+    );
+}
+
+PageWrapper.propTypes = {
+    children: PropTypes.node,
+}
+
+export default PageWrapper;
