bdfd86811c88b169201ac4d170157eeef53b81f2
Danny Berrios <denny_berrios@hotmail.com>
2024-12-18 23:29:57 -0300

    Adicionando nova rota ao RoutersApp

--- src/routers/index.jsx
+++ src/routers/index.jsx
@@ -2,12 +2,14 @@ import { Routes, Route } from "react-router-dom";
 import Home from "../views/Home";
 import PageNotFound from "../views/PageNotFound";
 import PageWrapper from "../components/PageWrapper";
+import NoteBoard from "../views/NoteBoard";
 
 const RoutersApp = () => {
     return (
         <PageWrapper>
             <Routes>
                 <Route path="/" element={<Home />} />
+                <Route path="/note-board" element={<NoteBoard />}/>
                 <Route path="*" element={<PageNotFound />} />
             </Routes>
         </PageWrapper>
